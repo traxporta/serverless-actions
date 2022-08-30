dc4b5de1b4f2d16095bd8b4b32e9dad89c4dd8b2
Alejandro Hernandez <a.hernandez@traxion.global>
2022-08-30 00:21:03 -0500

    feat: config github access token

--- common/git-setup.sh
+++ common/git-setup.sh
@@ -1,4 +1,8 @@
+#!/bin/bash -ex
+
 git config credential.helper store
+
+echo -e "https://tec-traxporta:$ACCESS_TOKEN@github.com" | sudo tee -a /etc/git-credentials
 
 echo -e "[url \"https://$ACCESS_TOKEN@github.com/\"]\n\tinsteadOf = ssh://git@github.com/" | sudo tee -a ~/.gitconfig
 
