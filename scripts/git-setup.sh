6622ca207f3abd0dc5f1a5c287a85f7c303cd11b
Alejandro Hernandez <a.hernandez@traxion.global>
2022-08-30 00:44:01 -0500

    fix: Changing file permissions

--- scripts/git-setup.sh
+++ scripts/git-setup.sh
@@ -2,8 +2,8 @@ git config credential.helper store
 
 echo -e "https://tec-traxporta:$ACCESS_TOKEN@github.com" | sudo tee -a /etc/git-credentials
 
-echo -e "[url \"https://$ACCESS_TOKEN@github.com/\"]\n\tinsteadOf = git@github.com/" | sudo tee -a ~/.gitconfig
+echo -e "[url \"https://tec-traxporta:$ACCESS_TOKEN@github.com/\"]\n\tinsteadOf = git@github.com/" | sudo tee -a ~/.gitconfig
 
-echo -e "[url \"https://$ACCESS_TOKEN@github.com/\"]\n\tinsteadOf = ssh://git@github.com/" | sudo tee -a ~/.gitconfig
+echo -e "[url \"https://tec-traxporta:$ACCESS_TOKEN@github.com/\"]\n\tinsteadOf = ssh://git@github.com/" | sudo tee -a ~/.gitconfig
 
-echo -e "[url \"https://$ACCESS_TOKEN@github.com/\"]\n\tinsteadOf = git+ssh://git@github.com/" | sudo tee -a ~/.gitconfig
+echo -e "[url \"https://tec-traxporta:$ACCESS_TOKEN@github.com/\"]\n\tinsteadOf = git+ssh://git@github.com/" | sudo tee -a ~/.gitconfig
